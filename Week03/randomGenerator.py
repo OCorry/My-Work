c8c4fdfb8f78d112d0a0bc2836cd2593862227bb
Orla <G00411336@gmit.ie>
2022-02-06 16:25:39 +0000

    Week03

---
+++ Week03/randomGenerator.py
@@ -0,0 +1,10 @@
+#Program that prints out a random number between 1 and 10
+#Author: Orla Corry 
+#Need to import the module random 
+
+import random 
+number = random.randint(1,10)
+print("here is a random number {}" .format(number))
+
+import random 
+print(random.randrange(0, 999))
