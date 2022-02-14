aa0b9ae31988bdcd57c1323be9d11bf27bc57b3f
Orla <G00411336@gmit.ie>
2022-02-14 21:23:40 +0000

    Week04-flow

--- Week04-flow/guess2.py
+++ Week04-flow/guess2.py
@@ -1,11 +1,16 @@
-numberToGuess = 30
+#program to gengerate a random number between 0 and 100 to guess
+#author: Orla Corry 
 
 guess = int(input("Please guess the number:"))
+import random
+numberToGuess = random.randint (1, 100)
+
 while guess != numberToGuess:
     if guess < numberToGuess:
         print("too Low")
     else:
         print("too high")
     guess = int(input("Please guess again:"))
+
 
 print("Well done! Yes the number was" , numberToGuess)
