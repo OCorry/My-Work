07f66fd380fc439529a0e7e22bf83d4ae4e74ead
Orla <G00411336@gmit.ie>
2022-02-20 15:36:44 +0000

    Week05

--- Week05-labs/Topic05-datastructures/student.py
+++ Week05-labs/Topic05-datastructures/student.py
@@ -2,19 +2,20 @@
 #The program should then output her data
 #Author: Orla Corry 
 
-student ={
-    "name" : "Mary",
-    "modules" : [
+student ={ #using {to open the dict
+    "name" : "Mary", #dict
+    "modules" : [       #using [ to open the list of dicts
         {"courseName" : "Programming",
-        "grade": 45
+        "grade": 45         #dict within a dict (using {} to show start and finish of 'sub dict')
         },
         {"courseName" :"History",
-        "grade" : 99
+        "grade" : 99        #dict within a dict (using {} to show start and finish of 'sub dict')
         }
-    ]
-}
+    ]   #using ] to close the list of dicts
+}       #using } to close the  original Student dict 
 
 print ("Student: {}" .format(student["name"]))
 for module in student ["modules"]:
     print (" \t {} \t : {}" .format(module["courseName"], module["grade"]))
+
 
