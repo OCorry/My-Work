96395a862487ea2e780cd6dd1bf30e7fe8b3b695
Orla <G00411336@gmit.ie>
2022-02-20 19:34:27 +0000

    weekday_draft

---
+++ Week05-labs/Topic05-datastructures/messing.py
@@ -0,0 +1,8 @@
+from winreg import HKEY_LOCAL_MACHINE
+
+
+x = 2+3
+print(x)
+
+y = "hello"
+print(y)
--- Week05-labs/Topic05-datastructures/student.py
+++ Week05-labs/Topic05-datastructures/student.py
@@ -1,21 +1,15 @@
-#Write a program that stores a student name and a list of her courses and grades in a dict
-#The program should then output her data
-#Author: Orla Corry 
 
-student ={ #using {to open the dict
-    "name" : "Mary", #dict
-    "modules" : [       #using [ to open the list of dicts
-        {"courseName" : "Programming",
-        "grade": 45         #dict within a dict (using {} to show start and finish of 'sub dict')
-        },
-        {"courseName" :"History",
-        "grade" : 99        #dict within a dict (using {} to show start and finish of 'sub dict')
-        }
-    ]   #using ] to close the list of dicts
-}       #using } to close the  original Student dict 
 
-print ("Student: {}" .format(student["name"]))
-for module in student ["modules"]:
-    print (" \t {} \t : {}" .format(module["courseName"], module["grade"]))
+
+       
+
+
+
+
+
+
+
+
+
 
 
