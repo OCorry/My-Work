826a5544730842995f1bfc31b214717e13475f82
Orla <G00411336@gmit.ie>
2022-03-13 15:54:04 +0000

    Number-of-e's

--- week07-task/es.py
+++ week07-task/es.py
@@ -6,23 +6,25 @@
 
 import sys 
 
-    #with open(sys.argv[1], 'rt') as f:
-    #data = f.read()  #using f.read() as a variable so it can be changed more easily 
-    #print (data)
-    #https://www.tutorialspoint.com/How-to-read-a-file-from-command-line-using-Python
-    #I did this simply to make sure the file would read before moving on to researching the letter counting part of the task
+#with open(sys.argv[1], 'rt') as f:
+#data = f.read()  #using f.read() as a variable  data so it is more accessable
+#print (data)
+#https://www.tutorialspoint.com/How-to-read-a-file-from-command-line-using-Python
+#I did this simply to make sure the file would read before moving on to researching the letter counting part of the task
+
+
+filename = sys.argv[1]  #storing this in a variable called filename so it is more accessable
 
 def letterFrequency(f, letter): #defining the function
     
-    with open(sys.argv[1], 'rt') as f: #open the file in read text mode 
+    with open(filename, 'rt') as f: #open the file in read text mode 
  
     # store content of the file in a variable
-         numberOfEs = f.read()
+         numberOfEs = f.read() #storing what we are looking for in the file as a variable 
  
-         return numberOfEs.count(letter)  # calling the function
+         return numberOfEs.count(letter) 
+        #Calling the function to print out the number of lower case e's in the txt file
  
- 
-# calling the function to print out the number of e's in the text 
 print(letterFrequency('f', 'e')) #dont need to specifically define that it's lower case here as Python is case sensitive anyway 
 
 #https://www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
