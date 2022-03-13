aefa49d30c1cbad182441235366bf69c312c0f66
Orla <G00411336@gmit.ie>
2022-03-13 16:03:42 +0000

    Number-of-e's

--- week07-task/es.py
+++ week07-task/es.py
@@ -2,17 +2,16 @@
 #I am assuming that the client is looking for the number of lower case e's as it is lowecase in the request and they dont specify
 #Author: Orla Corry 
 
-#import sys #importing sys.argv will allow to take the filename from an argument on the command line
-
-import sys 
-
+#I did this first simply to make sure the file would read before moving on to researching the letter counting part of the task
+#import sys 
 #with open(sys.argv[1], 'rt') as f:
 #data = f.read()  #using f.read() as a variable  data so it is more accessable
 #print (data)
 #https://www.tutorialspoint.com/How-to-read-a-file-from-command-line-using-Python
-#I did this simply to make sure the file would read before moving on to researching the letter counting part of the task
 
 
+import sys #need to import sys to be able to read a file from command line
+#https://stackoverflow.com/questions/7439145/i-want-to-read-in-a-file-from-the-command-line-in-python
 filename = sys.argv[1]  #storing this in a variable called filename so it is more accessable
 
 def letterFrequency(f, letter): #defining the function
